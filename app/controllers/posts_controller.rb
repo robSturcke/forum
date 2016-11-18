11d02837b8dcbf6c73091df5c1a5380b1eb3fbba
Rob Sturcke <rs@rsvision.com>
2016-11-18 12:17:04 -0500

    post controller for create, edit and show etc

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,42 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "\"#{@post.title}\" was saved successfully."
+      redirect_to @post
+    else
+      flash.now[:alert] = "Unable to post, please try again"
+      render :new
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "\"#{@post.title}\" was updated successfully."
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error saving \"#{@post.title}\", please try again."
+      render :edit
+    end
   end
 end
